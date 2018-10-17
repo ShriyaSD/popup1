41ba28be29ef5519f8ec70871168e1ec049dddf9
Shriya Devadiga <shriya.devadiga29@gmail.com>
2018-10-17 15:26:43 +0530

    Initial commit

---
+++ popup.js
@@ -0,0 +1,21 @@
+/*
+function pop(){
+    var overlay = $('<div id="overlay"></div>');
+    overlay.show();
+    overlay.appendTo(document.body);
+    $('.popup').show();
+    $('.close').click(function(){
+        $('.popup').hide();
+        overlay.appendTo(document.body).remove();
+        return false;
+    });
+
+    $('.x').click(function(){
+        $('.popup').hide();
+        overlay.appendTo(document.body).remove();
+        return false;
+    });
+};*/
+function pop() {
+    window.location.href="index.html#popup1";
+}
